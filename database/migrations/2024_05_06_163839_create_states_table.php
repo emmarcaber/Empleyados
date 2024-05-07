f73a4f089ab1bce28b5943206e9c53e8e43b6887
Emmar Caber <caberemmar@gmail.com>
2024-05-07 18:00:15 +0800

    feat: delete timestamps and other migrations

--- database/migrations/2024_05_06_163839_create_states_table.php
+++ database/migrations/2024_05_06_163839_create_states_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('country_id')->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->timestamps();
         });
     }
 
--- database/migrations/2024_05_06_163908_create_cities_table.php
+++ database/migrations/2024_05_06_163908_create_cities_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('state_id')->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->timestamps();
         });
     }
 
