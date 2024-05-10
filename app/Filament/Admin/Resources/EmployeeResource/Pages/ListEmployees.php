9222fdd25d73b4497bcfe775ed11e08a54d8926b
Emmar Caber <caberemmar@gmail.com>
2024-05-10 23:12:50 +0800

    feat: add tabs with badges in employee resource

--- app/Filament/Admin/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Admin/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Admin\Resources\EmployeeResource\Pages;
 
-use App\Filament\Admin\Resources\EmployeeResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Admin\Resources\EmployeeResource;
+use App\Models\Employee;
 
 class ListEmployees extends ListRecords
 {
@@ -14,6 +17,22 @@ class ListEmployees extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make('All'),
+            'This Week' => Tab::make('This Week')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subWeek()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subWeek())->count()),
+            'This Month' => Tab::make('This Month')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subMonth()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subMonth())->count()),
+            'This Year' => Tab::make('This Year')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subYear()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subYear())->count()),
         ];
     }
 }
